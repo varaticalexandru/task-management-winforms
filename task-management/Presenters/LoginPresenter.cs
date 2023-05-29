b14a3ff68199b27f7ce02a503b7b10f9b60aea22
alex <varaticalexandru1@gmail.com>
2023-05-30 02:37:57 +0300

    add auth validation

--- task-management/Presenters/LoginPresenter.cs
+++ task-management/Presenters/LoginPresenter.cs
@@ -7,6 +7,7 @@ using task_management.Models;
 using task_management.Views;
 using task_management._Repositories;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace task_management.Presenters
 {
@@ -34,16 +35,20 @@ namespace task_management.Presenters
         // methods
         private void Register(object sender, EventArgs e)
         {
+            // validate username and password
+            if (!validateUsername() || !validatePassword())
+                return;
+
             ILoginRepository loginRepository = new LoginRepository(connectionString);
             bool? result = loginRepository.Register(this.view.Username, this.view.Password);
 
             if (result == true)
             {
-                MessageBox.Show("Register successful!");
+                MessageBox.Show("Register successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("Register failed!");
+                MessageBox.Show("Register failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -54,11 +59,17 @@ namespace task_management.Presenters
 
             if (result == true)
             {
-                MessageBox.Show("Login successful!");
+                MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                IMainView mainView = new MainView();
+                new MainPresenter(mainView, connectionString);
+
+                ((Form)mainView).Show();
+                ((Form)this.view).Hide();
             }
             else
             {
-                MessageBox.Show("Login failed!");
+                MessageBox.Show("Login failed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -72,6 +83,30 @@ namespace task_management.Presenters
             {
                 this.view.PasswordTextBox.PasswordChar = '⚫';
             }
+        }
+
+        private bool validateUsername()
+        {
+            if (this.view.Username.Length < 3)
+            {
+                MessageBox.Show("Username must be at least 3 characters!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool validatePassword()
+        {
+            String pattern = @"^(?=.*[A-Z])(?=.*[!@#$%^&*()-=_+{};:,.<>?]).{8,}$";
+
+            if (this.view.Password.Length < 8 || !Regex.IsMatch(this.view.Password, pattern))
+            {
+                MessageBox.Show("Password length must be at least 8, it must include at least one uppercase and one special character!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
         }
 
     }
